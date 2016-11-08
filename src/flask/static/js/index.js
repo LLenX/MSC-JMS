dce568b1f23371289b4307fe998ad411ce76ff98
worstprogrammerCN <657278923@qq.com>
2016-11-09 00:32:22 +0800

    ..

--- src/flask/static/js/index.js
+++ src/flask/static/js/index.js
@@ -136,10 +136,23 @@ $(function () {
     }
 
     var checkIfAllOptionsAreSelected = function(){
-        var options = $("option:checked");
-        for(var i = 0; i < options.length; i++){
-            if ($(options[i]).text() == "请选择")
-                return false;
+        var first_checkbox_is_checked = $("#first-line .checkbox input").is(":checked");
+        var second_checkbox_is_checked = $("#second-line .checkbox input").is(":checked");
+        if (!first_checkbox_is_checked && !second_checkbox_is_checked)
+            return false;
+        if (first_checkbox_is_checked){
+            var options = $("#first-line option:selected");
+            for(var i = 0; i < options.length; i++){
+                if ($(options[i]).text() == "请选择")
+                    return false;
+            }
+        }
+        if (second_checkbox_is_checked){
+            var options = $("#second-line option:selected");
+            for(var i = 0; i < options.length; i++){
+                if ($(options[i]).text() == "请选择")
+                    return false;
+            }
         }
         return true;
     }
