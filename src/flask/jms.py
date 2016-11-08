e409109a3b9dcbdb94fcef2ebae1c15427d36076
Alvin Xue <19786654@qq.com>
2016-11-08 17:25:16 +0800

    correct the path of css and js source in index.html

--- src/flask/jms.py
+++ src/flask/jms.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
-import os.path
+from flask import Flask, render_template
 
 jms_server = Flask(__name__)
 
@@ -11,24 +10,12 @@ def index():
 
 @jms_server.route('/upload_param', methods=['GET', 'POST'])
 def do_prediction():
-    return 'hahaha'
+    return 'predict ok'
 
 
 @jms_server.route('/upload_param', methods=['GET', 'POST'])
 def upload_file():
-    return 'hahaha'
-
-
-# temporary function
-@jms_server.route('/<path:filename>')
-def get_file(filename):
-    ext_tup = os.path.splitext(filename)
-    print(ext_tup)
-    if ext_tup[1] != '.css' and ext_tup[1] != '.js':
-        return redirect(url_for('do_prediction'))
-
-    return open(
-        os.path.join(jms_server.root_path, 'templates/', filename)).read()
+    return 'upload ok'
 
 
 jms_server.run(debug=True)
