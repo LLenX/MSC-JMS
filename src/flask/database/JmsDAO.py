8fa989fe7f21b052bd4e5cf08af22dd99b204878
hyb1996 <946994919@qq.com>
2016-11-10 12:16:34 +0800

    improve database object

---
+++ src/flask/database/JmsDAO.py
@@ -0,0 +1,41 @@
+import mysql.connector
+from JmsDataManager import JmsDataManager
+
+"""
+Jms Database Access Object
+数据库读写主要对象。
+"""
+
+
+class JmsDAO:
+    USER_NAME = 'test'
+    PASSWORD = 'password'
+
+    def __init__(self, db_name):
+        JmsDAO.create_db_if_not_exists(db_name)
+        self.db_conn = mysql.connector.connect(host="localhost", user=JmsDAO.USER_NAME, passwd=JmsDAO.PASSWORD,
+                                               db=db_name,
+                                               charset='utf8')
+
+    def get_data_helper(self, option_set):
+        """
+        获取选项的数据管理器
+        :param option_set: 选项
+        :return:
+        """
+        return JmsDataManager(option_set, self.db_conn)
+
+    def close(self):
+        """
+        关闭数据库连接
+        :return:
+        """
+        self.db_conn.close()
+
+    @staticmethod
+    def create_db_if_not_exists(db_name):
+        database = mysql.connector.connect(host="localhost", user=JmsDAO.USER_NAME, passwd=JmsDAO.PASSWORD)
+        cursor = database.cursor()
+        sql = 'CREATE DATABASE IF NOT EXISTS ' + db_name
+        cursor.execute(sql)
+        database.close()
--- src/flask/database/example.py
+++ src/flask/database/example.py
@@ -1,36 +1,17 @@
 # coding: utf-8
 
-import MySQLdb
-import xlsxwriter
-import xlrd
-from DbSheet import DbSheet
+from JmsDAO import JmsDAO
+from EpredOption import *
 
-USER_NAME = 'test'
-PASSWORD = 'password'
-DB_NAME = 'excel'
+dao = JmsDAO('data')
 
+option = AREA_ALL | DURATION_ALL_YEAR | TIME_FIRST_HALF
 
-def create_db_if_not_exists(db_name):
-    database = MySQLdb.connect(host="localhost", user=USER_NAME, passwd=PASSWORD)
-    cursor = database.cursor()
-    sql = 'CREATE DATABASE IF NOT EXISTS ' + db_name
-    cursor.execute(sql)
-    database.close()
+dataHelper = dao.get_data_helper(option)
+dataHelper.prepare_input_files('D:\\data\\')
 
+# 这里调用EpredCaller运行epred
 
-create_db_if_not_exists(DB_NAME)
+# dataHelper.collect_output_files('D:\\result\\')
 
-db = MySQLdb.connect(host="localhost", user=USER_NAME, passwd=PASSWORD, db=DB_NAME, charset='utf8')
-
-readable_sheet = xlrd.open_workbook(u'D:\\宏观数据.xlsx', encoding_override="utf-8").sheets()[0]
-
-workbook = xlsxwriter.Workbook(r'D:\copy.xlsx')
-writable_sheet = workbook.add_worksheet()
-
-db_sheet = DbSheet(db, 'test')
-db_sheet.drop_table_if_exists()
-db_sheet.transfer_excel_to_db(readable_sheet)
-db_sheet.transfer_db_to_excel(writable_sheet)
-
-workbook.close()
-db.close()
+dao.close()
