07ad81e4de33aac2461aaae4789b8739340e358d
Alvin Xue <19786654@qq.com>
2016-11-11 16:22:54 +0800

    ready to program :)

--- src/flask/load_data_into_db.py
+++ src/flask/load_data_into_db.py
@@ -2,5 +2,5 @@
 from jmsserver.database.JmsDAO import JmsDAO
 
 if __name__ == '__main__':
-    jms_data_obj = JmsDAO(db_name='jms', username='root', password='xx')
+    jms_data_obj = JmsDAO(db_name='jms', username='root', password='AllenX12')
     jms_data_obj.import_all_input_files('jmsserver/epred/input_data/')
